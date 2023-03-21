24ef6d6394e950db8aa3684304e335d22d074c91
Venediktov <venediktov.vladimir.s@gmail.com>
2023-03-22 01:07:23 +0300

    micrometer, new Counter example

--- demo-spring-project/src/main/java/ru/venediktov/testspringproject/metrics/CalendarEventsMetric.java
+++ demo-spring-project/src/main/java/ru/venediktov/testspringproject/metrics/CalendarEventsMetric.java
@@ -1,35 +1,72 @@
 package ru.venediktov.testspringproject.metrics;
 
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
-import java.util.stream.IntStream;
-import javax.annotation.PostConstruct;
+import io.micrometer.core.instrument.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CalendarEventsMetric {
 
+  private Counter octoberCounter;
+  private Counter novemberCounter;
   private final MeterRegistry registry;
 
-  public void countEvents(int octoberIncrement, int novemberIncrement) {
-    Counter octoberCounter = Counter.builder("monthly_stats")
-        .tag("month", "november")
-        .register(registry);
-    Counter novemberCounter = Counter.builder("monthly_stats")
-        .tag("month", "october")
-        .register(registry);
+  @PostConstruct
+  public void init() {
+    octoberCounter = Counter.builder("monthly_stats")
+            .tag("month", "october")
+            .register(registry);
+    novemberCounter = Counter.builder("monthly_stats")
+            .tag("month", "november")
+            .register(registry);
+    countEvents(1,1);
 
+    // можно получить существующий counter (либо создастся новый)
+    Meter.Id id = octoberCounter.getId();
+    Counter counter = registry.counter("monthly_stats", "month", "october");
+    Meter.Id foundId = counter.getId();
+    log.info("Счетчик найден верно? - {}", foundId.equals(id));
+  }
+
+  public void countEvents(int octoberIncrement, int novemberIncrement) {
     IntStream.of(0, octoberIncrement)
         .forEach(octoberCounter::increment);
     IntStream.of(0, novemberIncrement)
         .forEach(novemberCounter::increment);
   }
 
-  @PostConstruct
-  public void init() {
-    countEvents(1,1);
+  public void count(String operationType, String office) {
+    Tag operationTypeTag = Tag.of("operation_type", operationType);
+    Tag officeUuidTag = Tag.of("office", office);
+    Counter simple_doc_usage = registry.counter("simple_doc_usage", Arrays.asList(operationTypeTag, officeUuidTag));
+    simple_doc_usage.increment();
+  }
+
+  @Scheduled(fixedRate = 10000)
+  public void testCount() {
+    count("create", "Москва");
+    count("delete", "Краснодар");
+    count("update", "Новосибирск");
+  }
+
+  public void putInHistogram(int a) {
+    DistributionSummary distributionSummary = DistributionSummary.builder("summary")
+            .publishPercentiles(0.5, 0.8)
+            .baseUnit("int")
+            .register(registry);
+    distributionSummary.record(a);
   }
 
 }
