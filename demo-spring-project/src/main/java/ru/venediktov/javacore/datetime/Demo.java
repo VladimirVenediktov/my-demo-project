b7bc39bda15f1f6c1b238b7cd9bae942baebc7ab
Venediktov <venediktov.vladimir.s@gmail.com>
2023-10-11 12:44:56 +0300

    Java Date Time (Instant/LocalDateTime/ZoneDateTime) examples

--- demo-spring-project/src/main/java/ru/venediktov/javacore/datetime/Demo.java
+++ demo-spring-project/src/main/java/ru/venediktov/javacore/datetime/Demo.java
@@ -1,8 +1,30 @@
 package ru.venediktov.javacore.datetime;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 public class Demo {
 
     public static void main(String[] args) {
+        Instant arrivalPlanDate = Instant.parse("2021-10-15T06:00:00Z");
+        System.out.println("Primary Instant " + arrivalPlanDate);
+
+        LocalDateTime localDateTimeUTC = LocalDateTime.ofInstant(arrivalPlanDate, ZoneOffset.UTC);
+        System.out.println("LocalDateTime UTC-0 " + localDateTimeUTC);
+
+        ZonedDateTime zonedDateTime = localDateTimeUTC.atZone(ZoneId.of("Asia/Novosibirsk"));
+        System.out.println("ZonedDateTime в часовом поясе юзера " + zonedDateTime);
+
+        ZonedDateTime serverZonedDateTime = zonedDateTime.withZoneSameInstant(ZoneId.systemDefault());
+        System.out.println("ZonedDateTime в часовом поясе сервера " + serverZonedDateTime);
+
+        LocalDateTime localDateTimeServer = serverZonedDateTime.toLocalDateTime();
+        System.out.println("ZonedDateTime в часовом поясе сервера " + localDateTimeServer);
+
+        System.out.println("\nОтдельный пример:");
         DateTime.dateConversion();
     }
 }
